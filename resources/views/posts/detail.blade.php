0773f0a5f70485bc839fd7c94dc3f1db2dbfdf38
yasuhitotanaka <sitofourth02@gmail.com>
2018-11-27 14:51:19 +0700

    change to open user_id and url with another tab

--- resources/views/posts/detail.blade.php
+++ resources/views/posts/detail.blade.php
@@ -4,11 +4,11 @@
   <div class="card">
     <div class="card-header text-white">{{ $post->title }}</div>
     <div class="card-body">
-    <h5 class="card-title text-white"><a href="{{ $post->url }}">{{ $post->url }}</a></h5>
+    <h5 class="card-title text-white"><a href="{{ $post->url }}" target="_blank">{{ $post->url }}</a></h5>
       <p class="card-text">{{ $post->description }}</p>
     </div>
     <div class="card-footer">
-    <a class="text-white" href='https://twitter.com/{{ $post->user }}'>@ {{ $post->user }}</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a class="text-white" href='https://twitter.com/{{ $post->user }}' target="_blank">@ {{ $post->user }}</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <span class="text-white">{{ $post->created_at }}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <a class="btn btn-primary btn-sm" href="{!! action('postsController@edit', ['post_id' => $post->id]) !!}">編集する</a>
     </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,11 +16,11 @@
         <a class="text-white" href="{!! action('postsController@show', ['id' => $post->id]) !!}">{{ $post->title }}</a>
       </div>
       <div class="card-body">
-        <h5 class="card-title text-blue"><a href="{{ $post->url }}">{{ $post->url }}</a></h5>
+        <h5 class="card-title text-blue"><a href="{{ $post->url }}" target="_blank">{{ $post->url }}</a></h5>
         <p class="card-text">{{ $post->description }}</p>
       </div>
       <div class="card-footer">
-        <a class="text-white" href='https://twitter.com/{{ $post->user }}'>@ {{ $post->user }}</a>
+        <a class="text-white" href='https://twitter.com/{{ $post->user }}' target="_blank">@ {{ $post->user }}</a>
       </div>
     </div>
     @endforeach
